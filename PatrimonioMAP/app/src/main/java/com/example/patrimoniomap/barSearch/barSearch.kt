165377b037838eddfed62e5164a359811a75d9a6
A01706202 <A01706202@itesm.mx>
2021-09-03 22:39:21 -0500

    Codigo

--- PatrimonioMAP/app/src/main/java/com/example/patrimoniomap/barSearch/barSearch.kt
+++ PatrimonioMAP/app/src/main/java/com/example/patrimoniomap/barSearch/barSearch.kt
@@ -1,31 +1,32 @@
-package com.example.patrimoniomap
+package com.example.patrimoniomap.barSearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.patrimoniomap.R
 
-
-class MainActivity : AppCompatActivity() {
+class barSearch : AppCompatActivity() {
 
     private lateinit var adapter: ArrayAdapter<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.bar_search)
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.test_array))
         lv_listView.adapter = adapter
-        lv_listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+        lv_listView.onItemClickListener = AdapterView.OnItemClickListener {parent, view, position, id ->
             Toast.makeText(applicationContext, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
         }
         lv_listView.emptyView = tv_emptyTextView
 
 
-    }
+        }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.nav_bar_search,menu)
@@ -47,4 +48,5 @@ class MainActivity : AppCompatActivity() {
 
         return super.onCreateOptionsMenu(menu)
 
+    }
 }
