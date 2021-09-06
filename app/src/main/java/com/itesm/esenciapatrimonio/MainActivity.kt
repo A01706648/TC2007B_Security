55fc5e438de863a72262681e6dfbfda445c36b92
DanielCruzAr <daniel_cruzar@outlook.com>
2021-09-05 23:55:55 -0500

    top app bar design

--- app/src/main/java/com/itesm/esenciapatrimonio/MainActivity.kt
+++ app/src/main/java/com/itesm/esenciapatrimonio/MainActivity.kt
@@ -2,10 +2,15 @@ package com.itesm.esenciapatrimonio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
