d8503e631adeec53ab15becc9b5f3aae428ed6b4
DanielCruzAr <daniel_cruzar@outlook.com>
2021-09-13 19:21:06 -0500

    vista principal y menu lateral

--- app/src/main/java/com/itesm/esenciapatrimonio/MainActivity.kt
+++ app/src/main/java/com/itesm/esenciapatrimonio/MainActivity.kt
@@ -1,36 +1,70 @@
 package com.itesm.esenciapatrimonio
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.navigation.NavigationView
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+    //mapBox
     private var mapView: MapView? = null
+    //navigation view
+    private lateinit var topAppBar: Toolbar
+    private lateinit var drawerLayout:DrawerLayout
+    private lateinit var navigationView:NavigationView
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //Mapbox
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
 
         setContentView(R.layout.activity_main)
+        /**
+         * Drawer Layout
+         **/
+        topAppBar = findViewById(R.id.topAppBar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
 
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            drawerLayout.closeDrawer(GravityCompat.END)
+            true
+        }
+
+        /**
+         * Mapbox
+        **/
         mapView = findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
         mapView?.getMapAsync { mapboxMap ->
 
             mapboxMap.setStyle(Style.MAPBOX_STREETS) {
 
-// Map is set up and the style has loaded. Now you can add data or make other map adjustments
-            Log.d("salida","mapa cargado")
+        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                Log.d("salida","mapa cargado")
 
             }
 
         }
+
     }
+
     override fun onStart() {
         super.onStart()
         mapView?.onStart()
