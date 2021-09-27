b7c9c609be68ae6484f3d1a07ffada57441cabcd
DanielCruzAr <daniel_cruzar@outlook.com>
2021-09-27 12:53:35 -0500

    interfaces

--- app/src/main/java/com/itesm/esenciapatrimonio/ui/MapFragment.kt
+++ app/src/main/java/com/itesm/esenciapatrimonio/ui/MapFragment.kt
@@ -1,10 +1,15 @@
 package com.itesm.esenciapatrimonio.ui
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.itesm.esenciapatrimonio.R
 import com.itesm.esenciapatrimonio.databinding.FragmentMapBinding
 import com.mapbox.mapboxsdk.Mapbox
@@ -13,6 +18,7 @@ import com.mapbox.mapboxsdk.maps.MapView
 class MapFragment: Fragment() {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+    //components
     private var mapView: MapView? = null
 
     override fun onCreateView(
@@ -29,19 +35,24 @@ class MapFragment: Fragment() {
         mapView = binding.mapView
         mapView?.onCreate(savedInstanceState)
         mapView?.getMapAsync { mapboxMap ->
-
             mapboxMap.setStyle(getString(R.string.map_style)) {
                 // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
             }
-
+            //todo 1) syncronize with the data base
+            //todo 2) for coordinate_x, coordinate_y in ResstoredSite table add a marker
+            //todo 3) set an event for each marker onClick
+            mapboxMap.addMarker(com.mapbox.mapboxsdk.annotations.MarkerOptions()
+                .position(com.mapbox.mapboxsdk.geometry.LatLng(20.693474, -100.471939))
+                .title("Juriquilla QRO"))
         }
 
         return root
-
-
     }
 
+    /**
+     * Map Lidecycle Methods
+     */
     override fun onStart() {
         super.onStart()
         mapView?.onStart()
