fe12e2003a926b133674fe755f695f1ed5e92cb2
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-09-29 18:30:04 -0500

    Nuevas interfaces y modificaciones a las interfaces

--- app/src/main/java/com/itesm/esenciapatrimonio/MainActivity.kt
+++ app/src/main/java/com/itesm/esenciapatrimonio/MainActivity.kt
@@ -16,32 +16,19 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.itesm.esenciapatrimonio.databinding.ActivityMainBinding
 import com.mapbox.mapboxsdk.maps.MapView
-<<<<<<< HEAD
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.Style
-
-class MainActivity : AppCompatActivity() {
-    //mapBox
-    private var mapView: MapView? = null
-    private var mapboxMap: MapboxMap? = null
-=======
 
 class MainActivity : AppCompatActivity() {
     //mapBox
 
 
->>>>>>> 41c5237347e8936e22e073589f7d05c763d30e6b
     //navigation view
     private lateinit var topAppBar: Toolbar
     private lateinit var drawerLayout:DrawerLayout
     private lateinit var navigationView:NavigationView
 
-<<<<<<< HEAD
-=======
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
->>>>>>> 41c5237347e8936e22e073589f7d05c763d30e6b
     /**
      * Android onCreate
      */
@@ -50,62 +37,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //Mapbox
 
-<<<<<<< HEAD
-        setContentView(R.layout.activity_main)
-        initializeComponents()
-
-        /**
-         * Mapbox initialize
-         **/
-        mapView = findViewById(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-
-            mapboxMap.setStyle(getString(R.string.map_style)) {
-                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-
-            }
-
-        }
-    }
-
-    /**
-     * Drawer Layout
-     **/
-    private fun initializeComponents(){
-        topAppBar = findViewById(R.id.topAppBar)
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navigationView = findViewById(R.id.navigation_view)
-        topAppBar.setNavigationOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
-
-        //compact activity
-
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            // Handle menu item selected
-            menuItem.isChecked = true
-
-            //R.id.view_acercaDeNosotros -> { setContentView(R.layout.about_us) }
-
-            drawerLayout.closeDrawer(GravityCompat.END)
-            true
-        }
-
-    }
-
-    /**
-     * Mapbox components
-     */
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
-=======
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
@@ -115,7 +46,6 @@ class MainActivity : AppCompatActivity() {
         /**
          * Mapbox initialize
          **/
->>>>>>> 41c5237347e8936e22e073589f7d05c763d30e6b
     }
 
     /**
