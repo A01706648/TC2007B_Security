370b2f1e329bb77177d82dad19a044e0378a32b7
A01706202 <A01706202@itesm.mx>
2021-10-04 12:18:47 -0500

    Correccion Restored Site

--- app/src/main/java/com/itesm/esenciapatrimonio/RestoredSiteActivity.kt
+++ app/src/main/java/com/itesm/esenciapatrimonio/RestoredSiteActivity.kt
@@ -1,11 +1,8 @@
 package com.itesm.esenciapatrimonio
 
-<<<<<<< HEAD
+
 import androidx.appcompat.app.AppCompatActivity
 
 class RestoredSiteActivity : AppCompatActivity() {
-    
-=======
-class RestoredSiteActivity {
->>>>>>> af92b7af4405c21ad808af42806b725faac654be
+
 }
