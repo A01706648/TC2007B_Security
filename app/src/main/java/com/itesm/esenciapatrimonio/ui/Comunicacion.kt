0e9a175cb3d138ed8dd7570feef4a46850461df8
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2021-10-13 10:20:15 -0500

    Errores

--- app/src/main/java/com/itesm/esenciapatrimonio/ui/Comunicacion.kt
+++
@@ -1,7 +0,0 @@
-package com.itesm.esenciapatrimonio.ui
-
-interface Comunicacion {
-    fun toggleProgressBar(status: Boolean)
-    fun lanzarActividad(tipoActividad: Class<*>?)
-    fun showMessage(msg: String?)
-}
